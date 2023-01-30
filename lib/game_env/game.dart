54cb0d767ab281c1a388cfa229391a8d697bb8c1
Abdel <serghiinc@gmail.com>
2023-01-30 11:37:59 +0100

    Added Flame Engine, Some ideas around using Sprites as plants

--- lib/game_env/game.dart
+++ lib/game_env/game.dart
@@ -1,10 +1,17 @@
+import 'dart:developer';
+
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/extensions.dart';
 import 'package:flame/game.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 
 /// A component that renders the crate sprite, with a 16 x 16 size.
 class Plant extends SpriteComponent {
-  Plant() : super(size: Vector2.all(28));
+  int stage;
+
+  Plant({required this.stage}) : super(size: Vector2.all(28));
 
   @override
   Future<void> onLoad() async {
@@ -12,32 +19,76 @@ class Plant extends SpriteComponent {
   }
 }
 
-class Floor extends SpriteComponent {
+class Floor extends SpriteComponent  with Tappable, Notifier{
   Floor() : super(size: Vector2.all(48));
+
+  bool _beenPressed = false;
 
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load('grid/120px-Flooring_29.png');
   }
-}
 
-// Game environement
-class MyGame extends FlameGame {
   @override
-  Future<void> onLoad() async {
-    double pixels = 48;
-    int x = 1;
-    int y = 2;
-
-    await add(PositionComponent(
-      position: Vector2(pixels*x,pixels*y),
-      children: [
-        Floor(),
-        Plant()
-      ]
-    ));
+  bool onTapUp(TapUpInfo info) {
+    _beenPressed = false;
+    return true;
   }
 
   @override
-  Color backgroundColor() => Colors.white;
+  bool onTapDown(_) {
+    _beenPressed = true;
+    return true;
+  }
+
+  @override
+  bool onTapCancel() {
+    _beenPressed = false;
+    return true;
+  }
+
+}
+
+// Game environement
+class MyGame extends FlameGame with HasTappables {
+
+  int _currentIndex = 0;
+  final int _offsetX = 10;
+  final int _offsetY = 128;
+  final double _pixels = 49;
+  final int _size = 8;
+
+  Future<void> _generateFloor() async {
+    for(int x=0; x<_size; x++) {
+      for(int y=0;y<_size;y++) {
+        await add(PositionComponent(
+          position: Vector2(_offsetX+_pixels*x, _offsetY+_pixels*y),
+            children: [
+              Floor(),
+            ]
+        ));
+      }
+    }
+  }
+
+  @override
+    Future<void> onLoad() async {
+      await _generateFloor();
+  }
+
+  @override
+  Color backgroundColor() => const Color(0xfff6dcc1);
+
+  void create() {
+    int currentX = _currentIndex%_size;
+    int currentY = _currentIndex~/_size;
+    add(PositionComponent(
+        position: Vector2(_offsetX+_pixels*currentX, _offsetY+_pixels*currentY),
+        children: [
+          Plant(stage: 1),
+        ]
+    ));
+
+    _currentIndex++;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flame/game.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 import 'game_env/game.dart';
 
@@ -11,18 +14,88 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final myGame = MyGame();
     return MaterialApp(
       title: "Pothos",
       home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text("Pothos")),
-          backgroundColor: Colors.green,
+          appBar: AppBar(
+            title: const Center(child: Text("Pothos")),
+            backgroundColor: Colors.green,
+          ),
+          body: const PothosBody()),
+    );
+  }
+}
+
+
+class PothosBody extends StatefulWidget {
+  const PothosBody({super.key});
+
+  @override
+  _PothosBodyState createState() => _PothosBodyState();
+}
+
+
+class _PothosBodyState extends State<PothosBody> {
+  late final MyGame game;
+
+  @override
+  void initState() {
+    super.initState();
+
+    game = MyGame();
+  }
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Stack(
+      children: [
+        Positioned.fill(
+            child: GameWidget(
+              game: game,
+            )),
+        Positioned(
+          left: 120,
+          top: 650,
+          child: ComponentsNotifierBuilder<Floor>(
+            notifier: game.componentsNotifier<Floor>(),
+            builder: (context, notifier) {
+              return GameHud(
+                emptyFloors: notifier.components.length, // TODO: change this
+                onCreate: game.create
+              );
+            },
+          ),
         ),
-        body: GameWidget(
-          game: myGame,
-        ),
+      ],
+    );
+  }
+}
+
+class GameHud extends StatelessWidget {
+  const GameHud({
+    super.key,
+    required this.emptyFloors,
+    required this.onCreate,
+  });
+
+  final int emptyFloors;
+  final VoidCallback onCreate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.transparent,
+      shadowColor: Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: emptyFloors > 0
+            ? ElevatedButton(
+          onPressed: onCreate,
+          child: const Text('Add a new plant'),
+        )
+            : Text('Remaining enemies: $emptyFloors'),
       ),
     );
   }
 }
+
